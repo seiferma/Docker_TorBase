6ccfbf6399c1a79842d2b6c31fc636ebdd428b22
Stephan Seifermann <seiferma@users.noreply.github.com>
2023-03-12 15:41:06 +0100

    support overriding uids/gids

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,39 @@
 #!/bin/sh
 
+# Create tor user account
+TOR_USER_NAME=tor-dckr
+TOR_GROUP_NAME=tor-dckr
+echo "Generating user account for tor"
+if [ "$TOR_GID" == "" ]; then
+	export TOR_GID=1500
+fi
+if [ "$TOR_UID" == "" ]; then
+	export TOR_UID=1500
+fi
+addgroup -g $TOR_GID $TOR_GROUP_NAME
+adduser -D -H -G $TOR_GROUP_NAME -u $TOR_UID $TOR_USER_NAME
+mkdir /home/$TOR_USER_NAME/
+chown $TOR_USER_NAME:$TOR_GROUP_NAME /home/$TOR_USER_NAME/
+
+# Create privoxy user account
+PRIVOXY_USER_NAME=privoxy-dckr
+PRIVOXY_GROUP_NAME=privoxy-dckr
+echo "Generating user account for privoxy"
+if [ "$PRIVOXY_GID" == "" ]; then
+	export PRIVOXY_GID=1501
+fi
+if [ "$PRIVOXY_UID" == "" ]; then
+	export PRIVOXY_UID=1501
+fi
+addgroup -g $PRIVOXY_GID $PRIVOXY_GROUP_NAME
+adduser -D -H -G $PRIVOXY_GROUP_NAME -u $PRIVOXY_UID $PRIVOXY_USER_NAME
+chown -R $PRIVOXY_USER_NAME:$PRIVOXY_GROUP_NAME /etc/privoxy/
+
 # Set DNS to localhost
 cat /etc/resolv.conf.tor > /etc/resolv.conf
 
 # Apply nft definitions
-echo 'define uid = '$(id -u tor) | cat - /etc/nftables.conf.template > /tmp/nftables.conf && mv /tmp/nftables.conf /etc/nftables.conf
+echo 'define uid = '$(id -u $TOR_USER_NAME) | cat - /etc/nftables.conf.template > /tmp/nftables.conf && mv /tmp/nftables.conf /etc/nftables.conf
 nft -f /etc/nftables.conf
 if [ $? -ne 0 ]; then
     echo "ERROR: Could not apply nftables definitions."
@@ -19,16 +48,17 @@ http_proxy=
 https_proxy=
 
 # Run tor under user "tor"
-su-exec tor tor -f /etc/tor/torrc > /tmp/tor.log &
+su-exec $TOR_USER_NAME tor -f /etc/tor/torrc > /tmp/tor.log &
 
 # Run privoxy under user "privoxy"
-su-exec privoxy privoxy --no-daemon /etc/privoxy/config &> /tmp/privoxy.log &
+su-exec $PRIVOXY_USER_NAME privoxy --no-daemon /etc/privoxy/config &> /tmp/privoxy.log &
 
 # restore http proxy variables
 export http_proxy=$http_proxy_old
 export https_proxy=$https_proxy_old
 
 # Wait for tor to be ready
+echo "Waiting for tor to become ready."
 DELAY=2
 TRIES=30
 TIMEOUT=$(( $DELAY * $TRIES ))
